33a5479d95e9594e27f4694660a275ab6b641750
ChanduAsani <asanichandra@gmail.com>
2021-09-05 14:04:01 +0530

    11

--- src/app/trated.service.ts
+++ src/app/trated.service.ts
@@ -10,19 +10,19 @@ export class TratedService {
   constructor(private hc:HttpClient) { }
 
   getUsers():Observable <any>{
-    return this.hc.get <any>('https://imdb-api.com/en/API/Top250TVs/k_e4ttl3ui');
+    return this.hc.get <any>('https://imdb-api.com/en/API/Top250TVs/k_2pm80c04');
   }
 
   getPopularTv():Observable <any>{
-    return this.hc.get <any>('https://imdb-api.com/en/API/MostPopularTVs/k_e4ttl3ui')
+    return this.hc.get <any>('https://imdb-api.com/en/API/MostPopularTVs/k_2pm80c04')
   }
 
   getTopMovies():Observable<any>{
-    return this.hc.get <any>("https://imdb-api.com/en/API/Top250Movies/k_e4ttl3ui")
+    return this.hc.get <any>("https://imdb-api.com/en/API/Top250Movies/k_2pm80c04")
   }
 
   getPopularMovies():Observable<any>{
-    return this.hc.get <any>("https://imdb-api.com/en/API/MostPopularMovies/k_e4ttl3ui")
+    return this.hc.get <any>("https://imdb-api.com/en/API/MostPopularMovies/k_2pm80c04")
   }
   
 }
